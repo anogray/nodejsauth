061f4131bf68d67a2b0c6da0c763e0762b39dde5
anogray <bbncr97@gmail.com>
2021-06-01 01:05:56 +0530

    added frontend ui

--- routes/mailer.js
+++ routes/mailer.js
@@ -44,7 +44,7 @@ const mailer = (req,res)=> {
                     let details = {email, password, otp:OTP}
                     // console.log("emailEr",info);
                     console.log("mailcookie",details,new Date())
-                    return res.cookie("details", details, { maxAge:30000, sameSite:"none", secure:true}).send();
+                    return res.cookie("details", details, { maxAge:30000}).send();
                     //return res.json({"Message from Server":"Please check OTP sent to the email above",info,"email":email})
                 }
                 });
