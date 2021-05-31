06245e72b3d249aa527e71819aa226c2482d1b28
anogray <bbncr97@gmail.com>
2021-05-31 17:45:54 +0530

    cors()

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   // origin: ["http://localhost:3000", "https://pensive-clarke-d769e6.netlify.app/", "https://master--pensive-clarke-d769e6.netlify.app/"],
-  origin: ["http://localhost:3000/, https://pensive-clarke-d769e6.netlify.app"],
+  origin: ['http://localhost:3000'],
   credentials: true
 }));
 app.use(function(req, res, next) {
