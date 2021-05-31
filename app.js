6827554edf42b889ff48a8edd94d1a6bff8182e5
anogray <bbncr97@gmail.com>
2021-06-01 01:38:43 +0530

    addign netifly to heroku origins

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   // origin: ["http://localhost:3000", "https://pensive-clarke-d769e6.netlify.app/", "https://master--pensive-clarke-d769e6.netlify.app/"],
-  origin: ['http://localhost:3000', "https://reactjsauth.vercel.app"],
+  origin: ['http://localhost:3000', "https://reactjsauth.vercel.app", "https://pensive-clarke-d769e6.netlify.app"],
   credentials: true
 }));
 
