38268a1591e3ddc9815fe1f2b5a121ba63322723
anogray <bbncr97@gmail.com>
2021-06-01 02:12:17 +0530

    testing http false and added samesite, secure

--- routes/httponly.js
+++ routes/httponly.js
@@ -5,7 +5,7 @@ httpRouter.get("/",(req,res)=>{
 
     console.log("got the httpOnly")
     let data = {dt:"apple"};
-    return res.cookie("htData", data,).send();
+    return res.cookie("htData", data, { sameSite:"none", secure:"auto"}).send();
 })
 
 module.exports = httpRouter;
